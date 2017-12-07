43f1c5c3cd9187a3c72423fffc8acd18dbd07d81
zhenhua.bao <zhenhua.bao@mokun.net>
2017-12-07 22:04:21 +0800

    Diligent

--- Source/UniqueSample/UniqueSample.cpp
+++ Source/UniqueSample/UniqueSample.cpp
@@ -3,7 +3,88 @@
 
 #include "stdafx.h"
 #include <SDL/SDL.h>
+#include <SDL/SDL_syswm.h>
+#include <memory>
+#include <iomanip>
+#include <Windows.h>
 
+#include "RefCntAutoPtr.h"
+#include "RenderDevice.h"
+#include "DeviceContext.h"
+#include "SwapChain.h"
+
+//#include <RenderDeviceFactoryD3D11.h>
+//#include <RenderDeviceFactoryD3D12.h>
+#include <RenderDeviceFactoryOpenGL.h>
+
+
+using namespace Diligent;
+
+// Create Direct3D device and swap chain
+void InitDevice(HWND hWnd, IRenderDevice **ppRenderDevice, IDeviceContext **ppImmediateContext, ISwapChain **ppSwapChain, DeviceType DevType)
+{
+	SwapChainDesc SCDesc;
+	SCDesc.SamplesCount = 1;
+	switch (DevType)
+	{
+#if false
+	case DeviceType::D3D11:
+	{
+		EngineD3D11Attribs DeviceAttribs;
+		DeviceAttribs.DebugFlags = (Uint32)EngineD3D11DebugFlags::VerifyCommittedShaderResources |
+			(Uint32)EngineD3D11DebugFlags::VerifyCommittedResourceRelevance;
+
+#ifdef ENGINE_DLL
+		GetEngineFactoryD3D11Type GetEngineFactoryD3D11 = nullptr;
+		// Load the dll and import GetEngineFactoryD3D11() function
+		LoadGraphicsEngineD3D11(GetEngineFactoryD3D11);
+#endif
+		auto *pFactoryD3D11 = GetEngineFactoryD3D11();
+		pFactoryD3D11->CreateDeviceAndContextsD3D11(DeviceAttribs, ppRenderDevice, ppImmediateContext, 0);
+		pFactoryD3D11->CreateSwapChainD3D11(*ppRenderDevice, *ppImmediateContext, SCDesc, hWnd, ppSwapChain);
+	}
+	break;
+
+	case DeviceType::D3D12:
+	{
+#ifdef ENGINE_DLL
+		GetEngineFactoryD3D12Type GetEngineFactoryD3D12 = nullptr;
+		// Load the dll and import GetEngineFactoryD3D12() function
+		LoadGraphicsEngineD3D12(GetEngineFactoryD3D12);
+#endif
+
+		auto *pFactoryD3D12 = GetEngineFactoryD3D12();
+		EngineD3D12Attribs EngD3D12Attribs;
+		EngD3D12Attribs.GPUDescriptorHeapDynamicSize[0] = 32768;
+		EngD3D12Attribs.GPUDescriptorHeapSize[1] = 128;
+		EngD3D12Attribs.GPUDescriptorHeapDynamicSize[1] = 2048 - 128;
+		EngD3D12Attribs.DynamicDescriptorAllocationChunkSize[0] = 32;
+		EngD3D12Attribs.DynamicDescriptorAllocationChunkSize[1] = 8; // D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER
+		pFactoryD3D12->CreateDeviceAndContextsD3D12(EngD3D12Attribs, ppRenderDevice, ppImmediateContext, 0);
+		pFactoryD3D12->CreateSwapChainD3D12(*ppRenderDevice, *ppImmediateContext, SCDesc, hWnd, ppSwapChain);
+	}
+	break;
+
+#endif
+	case DeviceType::OpenGL:
+	{
+#ifdef ENGINE_DLL
+		// Declare function pointer
+		GetEngineFactoryOpenGLType GetEngineFactoryOpenGL = nullptr;
+		// Load the dll and import GetEngineFactoryOpenGL() function
+		LoadGraphicsEngineOpenGL(GetEngineFactoryOpenGL);
+#endif
+		EngineCreationAttribs EngineCreationAttribs;
+		GetEngineFactoryOpenGL()->CreateDeviceAndSwapChainGL(
+			EngineCreationAttribs, ppRenderDevice, ppImmediateContext, SCDesc, hWnd, ppSwapChain);
+	}
+	break;
+
+	default:
+	//	LOG_ERROR_AND_THROW("Unknown device type");
+		break;
+	}
+}
 int done = 0;
 void
 loop()
@@ -40,6 +121,17 @@ loop()
 #endif
 }
 
+static HWND GetWindowHandle(SDL_Window* window)
+{
+	SDL_SysWMinfo sysInfo;
+
+	SDL_VERSION(&sysInfo.version);
+	SDL_GetWindowWMInfo(window, &sysInfo);
+	return sysInfo.info.win.window;
+}
+
+ISwapChain *g_pSwapChain;
+
 int
 main(int argc, char *argv[])
 {
@@ -71,12 +163,18 @@ main(int argc, char *argv[])
 	SDL_GL_CreateContext(window);
 #endif
 
-
+	RefCntAutoPtr<IRenderDevice> pRenderDevice;
+	RefCntAutoPtr<IDeviceContext> pDeviceContext;
+	Diligent::RefCntAutoPtr<ISwapChain> pSwapChain;
+	DeviceType DevType = DeviceType::OpenGL;
+	InitDevice(GetWindowHandle(window), &pRenderDevice, &pDeviceContext, &pSwapChain, DevType);
+	g_pSwapChain = pSwapChain;
 #ifdef __EMSCRIPTEN__
 	emscripten_set_main_loop(loop, 0, 1);
 #else
 	while (!done) {
 		loop();
+		pSwapChain->Present();
 	}
 #endif
 
